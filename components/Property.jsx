6a10ae5591224a91a9b73faf0fbae08fe4b4185f
Mootassam <mootassame@gmail.com>
2022-02-04 23:49:19 +0100

    Correct the coverPhoto is not undefined problem

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,7 +6,7 @@ import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
 import DefaultImage from "../assets/images/house.jpeg";
-const Property = {
+const Property = ({
   property: {
     coverPhoto,
     price,
@@ -19,49 +19,50 @@ const Property = {
     isVerified,
     externalID,
   },
-};
-<Link href={`/property/${externalId}`} passHref>
-  <Flex
-    flexWrap='wrap'
-    w='420px'
-    p='5'
-    padding
-    Top='0'
-    justifyContent='flex-start'
-    cursor='pointer'>
-    <Box>
-      <Image
-        src={coverPhoto ? coverPhoto.url : DefaultImage}
-        width={400}
-        height={260}
-        alt='house'
-      />
-    </Box>
-    <Box w='full'>
-      <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
-        <Flex alignItems='center'>
-          <Box paddingRight='3' color='green.400'>
-            {isVerified && <GoVerified />}
-          </Box>
-          <Text fontWeight='bold' fontSize='lg'>
-            AED {millify(price)}
-            {rentFrequency && `/${rentFrequency}`}
-          </Text>
+}) => (
+  <Link href={`/property/${externalID}`} passHref>
+    <Flex
+      flexWrap='wrap'
+      w='420px'
+      p='5'
+      padding
+      Top='0'
+      justifyContent='flex-start'
+      cursor='pointer'>
+      <Box>
+        <Image
+          src={coverPhoto ? coverPhoto.url : DefaultImage}
+          width={400}
+          height={260}
+          alt='house'
+        />
+      </Box>
+      <Box w='full'>
+        <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
+          <Flex alignItems='center'>
+            <Box paddingRight='3' color='green.400'>
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight='bold' fontSize='lg'>
+              AED {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
         </Flex>
-      </Flex>
-      <Flex
-        alignItems='center'
-        p='1'
-        justifyContent='space-between'
-        w='250px'
-        color='blue.400'>
-        {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
-        <BsGridFill />
-      </Flex>
-      <Text fontSize='lg'>
-        {title.length > 30 ? `${title.substring(0, 30)}...` : title}
-      </Text>
-    </Box>
-  </Flex>
-</Link>;
+        <Flex
+          alignItems='center'
+          p='1'
+          justifyContent='space-between'
+          w='250px'
+          color='blue.400'>
+          {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
+          <BsGridFill />
+        </Flex>
+        <Text fontSize='lg'>
+          {title.length > 30 ? `${title.substring(0, 30)}...` : title}
+        </Text>
+      </Box>
+    </Flex>
+  </Link>
+);
 export default Property;
