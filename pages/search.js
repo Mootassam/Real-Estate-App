e082a1061acd55c8a69973a83da3e18d6a984e00
Mootassam <mootassame@gmail.com>
2022-02-07 22:27:13 +0100

    noresult found

--- pages/search.js
+++ pages/search.js
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import { BsFilter } from "react-icons/bs";
 import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import noresult from "../assets/images/noresult.png";
 const Search = () => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
@@ -27,6 +29,19 @@ const Search = () => {
       <Text fontSize='2x1' p='4' fontWeight='bold'>
         Properties {router.query.purpose}
       </Text>
+      <Flex flexWrap='wrap'>
+        {[].map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+      {[].length === 0 && (
+        <Flex justifyContent='center' alignItems='center'>
+          <Image alt='no result' src={noresult} />
+          <Text fontSize='2xl' marginTop='3'>
+            No Results Found
+          </Text>
+        </Flex>
+      )}
     </Box>
   );
 };
